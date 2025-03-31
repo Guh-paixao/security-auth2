72933e9c74eb1ea29ba3fc239921300331007df0
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-03-31 17:44:53 -0300

    feat: add AdminUserConfig for initializing admin user and update database configurations

---
+++ src/main/java/br/com/gustavo/springsecurity/config/AdminUserConfig.java
@@ -0,0 +1,47 @@
+package br.com.gustavo.springsecurity.config;
+
+import br.com.gustavo.springsecurity.entities.Role;
+import br.com.gustavo.springsecurity.entities.User;
+import br.com.gustavo.springsecurity.repositories.RoleRepository;
+import br.com.gustavo.springsecurity.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("Admin já existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
+}
--- src/main/java/br/com/gustavo/springsecurity/config/SecurityConfig.java
+++ src/main/java/br/com/gustavo/springsecurity/config/SecurityConfig.java
@@ -7,11 +7,13 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -35,7 +37,11 @@ public class SecurityConfig {
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.POST, "/login").
+                        permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -52,5 +58,10 @@ public class SecurityConfig {
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
